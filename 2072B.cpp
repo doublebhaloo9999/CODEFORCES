42bbccab4ab319e6ab033a13a22ef1d53825c8ab
doublebhaloo9999 <202401138@daiict.ac.in>
2025-03-19 00:52:23 +0530

    Create 2072B.cpp

---
+++ 2072B.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve() {
+    ios_base::sync_with_stdio(0);	              
+    int n;
+    string s;
+    cin>>n>>s;
+    
+    
+
+
+}
+int main() {
+        ios_base::sync_with_stdio(0);
+        int t;    
+        cin>>t;
+
+        while(t--) {
+           solve();
+        }
+return 0;
+}
