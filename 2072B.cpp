4926e094dfaa2660dd0e7ea0cee9720762353df1
doublebhaloo9999 <202401138@daiict.ac.in>
2025-04-10 17:13:36 +0530

    Solved at last

--- 2072B.cpp
+++ 2072B.cpp
@@ -2,18 +2,17 @@
 using namespace std;
 void solve() {
     ios_base::sync_with_stdio(0);	              
-    int ans;
+    int64_t ans;
     string s;
     cin>>ans>>s;
 
-    int dash =0 , underscore = 0 ;
+    int64_t dash =0 , underscore = 0 ;
     for (int i = 0; i < s.size() ; i++) if(s[i] == '-') dash++ ;
     
     underscore = s.size() - dash ;
 
-    cout << "Dash = "<< dash << " Underscore = " << underscore << '\n';
+    // cout << "Dash = "<< dash << " Underscore = " << underscore << '\n';
 
-    ans = 0 ;
     ans = ( dash / 2 )*( dash - dash / 2) * underscore ;
 
     cout << ans << endl ;
